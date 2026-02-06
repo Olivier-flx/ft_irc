ffa990624a7009a9c8c69150972e3a54832a1bae
Olivier-flx <olivier.filloux@live.fr>
2026-02-06 15:47:07 +0100

    setup

---
+++ headers/ft_irc.hpp
@@ -0,0 +1,9 @@
+
+#ifndef FT_IRC_H
+# define FT_IRC_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+int	main()
+{
+	//create socket
+	//Bind the socket to a IP / port
+	// Mark the socket for listening in
+	// accept a call
+	// close the listening socket
+	// while reveiving display message , echo message
+	//close socket
+
+	return 0;
+}
